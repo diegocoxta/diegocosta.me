1a472fd22e1411ab38820431780845e63c8e2293
Patrick César <69088118+ptrickcesar@users.noreply.github.com>
2020-10-31 16:47:25 -0300

    Update Footer.tsx
    footer correction

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -76,9 +76,9 @@ export default function Footer(props: FooterProps): React.ReactElement {
             ))}
         </NavigationList>
       </Navigation>
-      <Label>© {props.year}, built with </Label>
+      <Label> © {props.year}, built with </Label>
       <Link href="https://gatsbyjs.org">gatsby</Link>
-      <Label> • </Label>
+      <Label>   • </Label>
       {props.repository && <Link href={props.repository}>source code</Link>}
     </Container>
   );
