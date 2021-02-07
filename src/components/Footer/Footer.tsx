cf80346d9057228adb87c0eb0d1929b75056832f
Diego Costa <diego@diegocosta.com.br>
2021-02-07 11:08:51 -0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -59,7 +59,7 @@ export default function Footer(props: FooterProps): React.ReactElement {
     <Container>
       <Navigation />
       <Content>
-        <Label>© {props.year}, built with </Label>
+        <Label>CC-BY {props.year}, built with </Label>
         <Link href="https://gatsbyjs.org">gatsby</Link>
         <Label> • </Label>
         {props.repository && <Link href={props.repository}>source code</Link>}
