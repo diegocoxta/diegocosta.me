2c6279f849dd1bfcd3d2114726acb750f7798073
Diego Costa <diego@diegocosta.com.br>
2021-05-29 16:37:16 -0300

    chage the main color

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -3,7 +3,7 @@ export default {
   subtitleColor: '#9a9a9a',
   textColor: '#FFFFFF',
   backgroundColor: '#0C0D0E',
-  accentColor: '#CA222B',
+  accentColor: '#1FB651',
   searchBackgroundColor: '#0e0f11',
   searchResultsBackgroundColor: '#1b1b1d',
 };
--- src/themes/light.ts
+++ src/themes/light.ts
@@ -3,7 +3,7 @@ export default {
   subtitleColor: '#737373',
   textColor: '#0C0D0E',
   backgroundColor: '#efefef',
-  accentColor: '#CA222B',
+  accentColor: '#1FB651',
   searchBackgroundColor: '#e0e0e0',
   searchResultsBackgroundColor: '#e0e0e0',
 };
