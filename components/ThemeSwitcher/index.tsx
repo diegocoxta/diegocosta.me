0cff9ac845211ca0beec2131d9605a8f0a37cb75
Diego Costa <diego@diegocosta.me>
2026-02-11 14:31:12 -0300

    fix: removed unused code from theme switcher

--- components/ThemeSwitcher/index.tsx
+++ components/ThemeSwitcher/index.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 
 import _ThemeSwitcher from './ThemeSwitcher';
 
 export default function ThemeSwitcher(): React.ReactElement {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
   const isDarkMode = theme === 'dark';
-
-  if (!mounted) {
-    return <_ThemeSwitcher isDarkMode={true} />;
-  }
 
   return <_ThemeSwitcher isDarkMode={isDarkMode} onClick={() => setTheme(isDarkMode ? 'default' : 'dark')} />;
 }
