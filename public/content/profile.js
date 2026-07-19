ea8047e75dcbbf48893b5c3ab68239554f71b9ac
Diego Costa <diego@diegocosta.me>
2026-07-19 03:12:09 -0300

    Revise bio for clarity and focus on key interests
    Updated bio to emphasize core interests in engineering leadership, people management, and diversity.

--- public/content/profile.js
+++ public/content/profile.js
@@ -5,7 +5,7 @@ const profile = {
   description: "Engineering Manager passionate about building products that impact people's lives.",
   icon: 'static/icon.png',
   author: 'Diego Costa',
-  bio: 'I’m an Engineering Manager passionate about turning technology into real impact. I have over 10 years of experience in software engineering, working across large companies and fast-growing startups.\nI have a great interest in engineering leadership, people management, software engineering, as well as inclusion and diversity.\nIf you’d like to learn more about me, check out the links below!',
+  bio: 'I’m an Engineering Manager passionate about turning technology into real impact. I have over 10 years of experience in software engineering, working across large companies and fast-growing startups.\nMy core interests include engineering leadership, people management, and fostering diversity and inclusion in the tech industry.\nIf you’d like to learn more about me, check out the links below!',
   links: [
     { label: 'Linkedin', url: 'https://linkedin.com/in/diegocoxta' },
     { label: 'Github', url: 'https://github.com/diegocoxta' },
